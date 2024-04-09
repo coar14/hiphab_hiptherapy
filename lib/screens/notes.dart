761c59832e0331ec2642d5d5b4ed74ad5ff65827
cor <corchxmasong@gmail.com>
2024-04-10 00:03:05 +0800

    ui of notes, not finished

--- lib/screens/notes.dart
+++ lib/screens/notes.dart
@@ -1,114 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:therapy/themes/app_image.dart';
+import 'package:therapy/themes/const_style.dart';
 
 class Notes extends StatelessWidget {
-  const Notes({super.key});
+  const Notes({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: ListView(
+          padding: EdgeInsets.symmetric(vertical: 20),
           scrollDirection: Axis.vertical,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal:10, vertical: 30),
-              child: Column(
-                children: [
-                  Text('Therapy',
-                  style: GoogleFonts.leagueSpartan(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color:const Color(0xFF58595B)  
-                   ),
-                  ),
-                  const SizedBox(height: 15),
-                  Stack(
-                  children: [
-                    Container(
-                      height: 269,
-                      width: 362,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFD9D9D9),
-                        borderRadius: BorderRadius.circular(40),
-                        image: const DecorationImage(
-                          image: AssetImage(AppImages.programs)
-                          )
-                      ),
-                    ),
-                    Positioned(
-                      top: 225,
-                      left: 120,
-                      child:Text('Programs',
-                    style: GoogleFonts.lato(
-                      fontWeight: FontWeight.w900,
+            Column(
+              children: [
+                Container(
+                  height: 170,
+                  width: 316,
+                  color: aRed,
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'THE HIP \nREPLACEMENT \nPOST-SURGERY \nTHERAPY',
+                    style: GoogleFonts.leagueSpartan(
                       fontSize: 24,
-                      color: Colors.white
-                    )
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                    ),
-                  ],
                   ),
-                  const SizedBox(height: 45),
-                  Stack(
-                  children: [
-                    Container(
-                      height: 269,
-                      width: 362,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFD9D9D9),
-                        borderRadius: BorderRadius.circular(40),
-                        image: const DecorationImage(
-                          image: AssetImage(AppImages.exercises)
-                          )
-                      ),
+                ),
+                SizedBox(height: 25),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'It is recommended you dedicate 30-60 minutes each \nday for exercises. You can break up your exercise \nroutine and do some exercises in the morning and \nothers in the afternoon or perform different groups of \nexercises on different days.',
+                    style: GoogleFonts.inter(
+                      fontSize: 12,
                     ),
-                    Positioned(
-                      top: 225,
-                      left: 120,
-                      child:Text('Exercises',
-                    style: GoogleFonts.lato(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 24,
-                      color: Colors.white
-                    )
-                    ),
-                    ),
-                  ],
+                    textAlign: TextAlign.justify,
                   ),
-                  const SizedBox(height: 45),
-                  Stack(
-                  children: [
-                    Container(
-                      height: 269,
-                      width: 362,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFD9D9D9),
-                        borderRadius: BorderRadius.circular(40),
-                        image: const DecorationImage(
-                          image: AssetImage(AppImages.plan)
-                          )
-                      ),
-                    ),
-                    Positioned(
-                      top: 225,
-                      left: 120,
-                      child:Text('Create your plan',
-                    style: GoogleFonts.lato(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 24,
-                      color: Colors.white
-                    )
-                    ),
-                    ),
-                  ],
+                ),
+                SizedBox(height: 35),
+                Container(
+                  height: 500,
+                  width: 500,
+                  color: Colors.grey,
+                  child: Text(
+                    'CATEGORIES',
+                    style: GoogleFonts.leagueSpartan(fontSize: 28),
+                    textAlign: TextAlign.center,
                   ),
-                ],
-              ),
-            )
+                )
+              ],
+            ),
           ],
-        )
+        ),
       ),
     );
   }
